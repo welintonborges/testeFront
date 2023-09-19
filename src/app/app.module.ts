aba0d2cc2f4d60d80f9765960b9e16ca1237698a
Welinton Borges <Welinton.b.borges@gmail.com>
2023-09-19 12:02:43 -0300

    new projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MainRoutingModule } from "./main/main-routing.module";
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, MainRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {FormularioComponent} from "../formulario/formulario.component";
+
+const routes: Routes = [
+  {path:'', component: FormularioComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MainRoutingModule { }
